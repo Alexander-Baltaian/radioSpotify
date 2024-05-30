7b6cb16f2055969f658ae290afc4d8f8544147b8
Alexander-Baltaian <122455868+Alexander-Baltaian@users.noreply.github.com>
2024-05-30 20:49:48 +0200

    push

--- script.js
+++ script.js
@@ -24,7 +24,7 @@ $( document ).ready(function() {
     let client_id = '958e609e6b454a83929bcb8b3ebb3646';
     // Use the following site to convert your regular url to the encoded version:
     // https://www.url-encode-decode.com/
-    let redirect_uri = 'https%3A%2F%2Fmujibsardar.github.io%2Fspotify_jquery_only'; // GitHub Pages URL or whatever your public url to this app is
+    let redirect_uri = 'https%3A%2F%2Falexander-baltaian.github.io%2FradioSpotify'; // GitHub Pages URL or whatever your public url to this app is
     // *************** END *************************
 
     const redirect = `https://accounts.spotify.com/authorize?client_id=${client_id}&response_type=token&redirect_uri=${redirect_uri}`;
