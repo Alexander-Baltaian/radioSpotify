18df98729e83e6deebd9b5312094e94915a375e4
Alexander-Baltaian <122455868+Alexander-Baltaian@users.noreply.github.com>
2024-05-30 20:40:16 +0200

    truc

--- script.js
+++ script.js
@@ -21,7 +21,7 @@ $( document ).ready(function() {
 
     // AUTHORIZE with Spotify (if needed)
     // *************** REPLACE THESE VALUES! *************************
-    let client_id = 'df6968c34f4b4494ac206c7628d2afc7';
+    let client_id = '958e609e6b454a83929bcb8b3ebb3646';
     // Use the following site to convert your regular url to the encoded version:
     // https://www.url-encode-decode.com/
     let redirect_uri = 'https%3A%2F%2Fmujibsardar.github.io%2Fspotify_jquery_only'; // GitHub Pages URL or whatever your public url to this app is
